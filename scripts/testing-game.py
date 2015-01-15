347f991869e3985713e4cfbe062660e98f528d78
Diego Cristina Capelo <dccapelo@spotify.com>
2015-01-15 17:22:21 +0100

    Testing-game: Also count @Test annotations
    Change-Id: If2bdc43ae64e50a37f3631a6e601ffbc0d42ff89
    
    Conflicts:
    	scripts/testing-game.py

--- scripts/testing-game.py
+++ scripts/testing-game.py
@@ -15,7 +15,7 @@ names = {}
 for root, dirs, files in os.walk(args.directory):
     for name in files:
         filename, fileextension = os.path.splitext(name)
-        
+
         absfile = os.path.join(root, name)
         
         if fileextension == '.m' or fileextension == '.mm':
@@ -39,18 +39,21 @@ for root, dirs, files in os.walk(args.directory):
             try:
                 with open(absfile) as sourcefile:
                     source = sourcefile.read()
-                    p = subprocess.Popen(['git', 'blame', absfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    p = subprocess.Popen(['git', 'blame', '-e', absfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                     out, err = p.communicate()
-                    
+
+                    next_is_test = False
                     for blame_line in out.splitlines():
-                        if blame_line.replace(' ', '').find('publicvoidtest') != -1:
-                            blame_info = blame_line[blame_line.find('(')+1:]
-                            blame_info = blame_info[:blame_info.find(')')]
-                            blame_components = blame_info.split()
-                            name_components = blame_components[:len(blame_components)-4]
-                            name = ' '.join(name_components)
+                        separator = blame_line.find(')')
+                        blame_code_nospaces = blame_line[separator+1:].replace(' ', '').replace('\t', '')
+                        if next_is_test or blame_code_nospaces.startswith('publicvoidtest'):
+                            blame_info = blame_line[:separator]
+                            name = blame_info[blame_info.find('<')+1:blame_info.find('@')]
                             name_count = names.get(name, 0)
                             names[name] = name_count + 1
+                            next_is_test = False
+                        else:
+                            next_is_test = blame_code_nospaces.startswith('@Test')
             except:
                 'Could not open file: ' + absfile
 
@@ -61,6 +64,7 @@ for name in names:
 print "Total Tests: %(t)d" % { 't' : total_tests }
 print "-------------------------------------------"
 sorted_list = sorted(names.items(), key=lambda x: x[1], reverse = True)
+
 for t in sorted_list:
     percentage = (float(t[1]) / float(total_tests)) * 100.0
-    print "%(i)d. %(n)s, %(t)d (%(p).2f%%)" % { 'i': sorted_list.index(t) + 1, 'n' : t[0], 't' : t[1], 'p' : percentage }
+    print "%(i)d. %(n)s, %(t)d (%(p).2f%%)" % { 'i': sorted_list.index(t) + 1, 'n' : name_mapping[t[0]], 't' : t[1], 'p' : percentage }

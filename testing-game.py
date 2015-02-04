18deb0203cca281d8ae3be5373ce9090759f0f4d
Will Sackfield <sackfield@spotify.com>
2015-02-04 10:17:20 +0100

    Refactor testing-game to be remove duplicate logic
    * Some of it at least

--- testing-game.py
+++ testing-game.py
@@ -11,6 +11,9 @@ parser.add_argument('-d', '--directory', help='The directory to search for files
 args = parser.parse_args()
 
 names = {}
+objc_extensions = [ '.m', '.mm' ]
+java_extensions = [ '.java' ]
+valid_extensions = objc_extensions + java_extensions
 
 for root, dirs, files in os.walk(args.directory):
     for name in files:
@@ -18,42 +21,37 @@ for root, dirs, files in os.walk(args.directory):
 
         absfile = os.path.join(root, name)
         
-        if fileextension == '.m' or fileextension == '.mm':
+        if fileextension in valid_extensions:
             try:
                 with open(absfile) as sourcefile:
                     source = sourcefile.read()
-                    if source.find('XCTestCase') != -1:
-                        p = subprocess.Popen(['git', 'blame', absfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                        out, err = p.communicate()
-                        
+                    if fileextension in objc_extensions:
+                        if source.find('XCTestCase') != -1:
+                            p = subprocess.Popen(['git', 'blame', absfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                            out, err = p.communicate()
+                            
+                            for blame_line in out.splitlines():
+                                if blame_line.replace(' ', '').find('-(void)test') != -1:
+                                    blame_info = blame_line[blame_line.find('(')+1:]
+                                    blame_info = blame_info[:blame_info.find(')')]
+                                    blame_components = blame_info.split()
+                                    name_components = blame_components[:len(blame_components)-4]
+                                    name = ' '.join(name_components)
+                                    name_count = names.get(name, 0)
+                                    names[name] = name_count + 1
+                    else:
+                        next_is_test = False
                         for blame_line in out.splitlines():
-                            if blame_line.replace(' ', '').find('-(void)test') != -1:
-                                blame_info = blame_line[blame_line.find('(')+1:]
-                                blame_info = blame_info[:blame_info.find(')')]
-                                blame_components = blame_info.split()
-                                name_components = blame_components[:len(blame_components)-4]
-                                name = ' '.join(name_components)
+                            separator = blame_line.find(')')
+                            blame_code_nospaces = blame_line[separator+1:].replace(' ', '').replace('\t', '')
+                            if next_is_test or blame_code_nospaces.startswith('publicvoidtest'):
+                                blame_info = blame_line[:separator]
+                                name = blame_info[blame_info.find('<')+1:blame_info.find('@')]
                                 name_count = names.get(name, 0)
                                 names[name] = name_count + 1
-        elif fileextension == '.java' and filename.endswith('Test'):
-            try:
-                with open(absfile) as sourcefile:
-                    source = sourcefile.read()
-                    p = subprocess.Popen(['git', 'blame', '-e', absfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                    out, err = p.communicate()
-
-                    next_is_test = False
-                    for blame_line in out.splitlines():
-                        separator = blame_line.find(')')
-                        blame_code_nospaces = blame_line[separator+1:].replace(' ', '').replace('\t', '')
-                        if next_is_test or blame_code_nospaces.startswith('publicvoidtest'):
-                            blame_info = blame_line[:separator]
-                            name = blame_info[blame_info.find('<')+1:blame_info.find('@')]
-                            name_count = names.get(name, 0)
-                            names[name] = name_count + 1
-                            next_is_test = False
-                        else:
-                            next_is_test = blame_code_nospaces.startswith('@Test')
+                                next_is_test = False
+                            else:
+                                next_is_test = blame_code_nospaces.startswith('@Test')
             except:
                 'Could not open file: ' + absfile
 
@@ -67,4 +65,4 @@ sorted_list = sorted(names.items(), key=lambda x: x[1], reverse = True)
 
 for t in sorted_list:
     percentage = (float(t[1]) / float(total_tests)) * 100.0
-    print "%(i)d. %(n)s, %(t)d (%(p).2f%%)" % { 'i': sorted_list.index(t) + 1, 'n' : name_mapping[t[0]], 't' : t[1], 'p' : percentage }
+    print "%(i)d. %(n)s, %(t)d (%(p).2f%%)" % { 'i': sorted_list.index(t) + 1, 'n' : t[0], 't' : t[1], 'p' : percentage }
